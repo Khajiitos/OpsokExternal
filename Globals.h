9b022e1792165c67e15d0590116273f1d188d600
Khajiitos <58109276+Khajiitos@users.noreply.github.com>
2020-11-29 12:10:54 +0100

    Update Globals.h

--- Globals.h
+++ Globals.h
@@ -5,6 +5,7 @@
 #include <TlHelp32.h>
 
 extern uintptr_t moduleBase;
+extern uintptr_t engineModule;
 extern DWORD procId;
 extern HWND hwnd;
 extern HANDLE hProcess;
@@ -26,7 +27,10 @@ struct Config {
 	bool glow = true;
 	bool autoBh = true;
 	bool triggerbot = true;
+	short triggerbotKey = VK_XBUTTON2;
 	bool radarHack = true;
+	bool aimbot = true;
+	short aimbotToggleKey = VK_XBUTTON1;
 };
 
 extern Config config;
