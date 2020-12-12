35eaff636f7386fc7e449d59d283f7818a894460
Khajiitos <58109276+Khajiitos@users.noreply.github.com>
2020-12-12 16:25:11 +0100

    Update Globals.h

--- Globals.h
+++ Globals.h
@@ -4,8 +4,8 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 
-extern uintptr_t moduleBase;
-extern uintptr_t engineModule;
+extern uintptr_t clientBase;
+extern uintptr_t engineBase;
 extern DWORD procId;
 extern HWND hwnd;
 extern HANDLE hProcess;
@@ -25,12 +25,16 @@ enum Team {
 
 struct Config {
 	bool glow = true;
+	float glowAlpha = 0.8f;
 	bool autoBh = true;
 	bool triggerbot = true;
 	short triggerbotKey = VK_XBUTTON2;
 	bool radarHack = true;
 	bool aimbot = true;
+	float aimbotFov = 255.f;
 	short aimbotToggleKey = VK_XBUTTON1;
+	bool aimbotControlRecoil = true;
+	bool aimbotVisibilityCheck = false;
 };
 
 extern Config config;
