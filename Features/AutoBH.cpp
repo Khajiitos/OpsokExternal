fc8a51f1b270c67166736af9269e269c96a41d04
Khajiitos <58109276+Khajiitos@users.noreply.github.com>
2020-12-12 16:26:44 +0100

    Update AutoBH.cpp

--- Features/AutoBH.cpp
+++ Features/AutoBH.cpp
@@ -1,21 +1,15 @@
 #include "AutoBH.h"
 #include "../Offsets.h"
 #include "../Globals.h"
-#include "../Vector.h"
 
 void AutoBH::run() {
 
 	if (!config.autoBh)
 		return;
 
-	uintptr_t localPlayer = mem.read<uintptr_t>(moduleBase + dwLocalPlayer);
-	Vector velocity = mem.read<Vector>(localPlayer + m_vecVelocity);
+	uintptr_t localPlayer = mem.read<uintptr_t>(clientBase + dwLocalPlayer);
 	bool onGround = mem.read<DWORD>(localPlayer + m_fFlags) & Flags::ON_GROUND;
 
-	if (GetAsyncKeyState(VK_SPACE)) {
-		if (onGround) {
-			mem.write<DWORD>(moduleBase + dwForceJump, 6);
-		}
-	}
-
+	if (GetAsyncKeyState(VK_SPACE) && onGround)
+		mem.write<DWORD>(clientBase + dwForceJump, 6);
 }
