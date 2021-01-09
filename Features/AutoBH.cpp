f769f6ff816aae5e34bee9d16783ca253e2346be
Khajiitos <58109276+Khajiitos@users.noreply.github.com>
2021-01-09 20:18:20 +0100

    Update AutoBH.cpp

--- Features/AutoBH.cpp
+++ Features/AutoBH.cpp
@@ -8,6 +8,10 @@ void AutoBH::run() {
 		return;
 
 	uintptr_t localPlayer = mem.read<uintptr_t>(clientBase + dwLocalPlayer);
+
+	if (!localPlayer)
+		return;
+
 	bool onGround = mem.read<DWORD>(localPlayer + m_fFlags) & Flags::ON_GROUND;
 
 	if (GetAsyncKeyState(VK_SPACE) && onGround)
