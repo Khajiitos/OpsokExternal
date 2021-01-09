479b7dffc7bdce2312af9b56d1d77bd7173e5a71
Khajiitos <58109276+Khajiitos@users.noreply.github.com>
2021-01-09 20:19:03 +0100

    Update Glow.h

--- Features/Glow.h
+++ Features/Glow.h
@@ -5,12 +5,20 @@ struct GlowStruct {
 	float green = 1.f;
 	float blue = 1.f;
 	float alpha = 1.f;
-	uint8_t padding[8];
+	char pad1[8];
 	float unknown = 1.f;
-	uint8_t padding2[4];
+	char pad2[4];
 	bool renderOccluded = true;
 	bool renderUnoccluded = false;
 	bool fullBloom = false;
+	char pad3[5];
+	int glowStyle;
+
+	GlowStruct(float r, float g, float b, float a)
+		: red(r), green(g), blue(b), alpha(a)
+	{
+
+	}
 };
 
 namespace Glow {
