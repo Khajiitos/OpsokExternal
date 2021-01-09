8180c62238a4a608de23344c4a560f6967857482
Khajiitos <58109276+Khajiitos@users.noreply.github.com>
2021-01-09 20:19:45 +0100

    Update Triggerbot.cpp

--- Features/Triggerbot.cpp
+++ Features/Triggerbot.cpp
@@ -10,11 +10,14 @@ void Triggerbot::run() {
 	if (!GetAsyncKeyState(config.triggerbotKey))
 		return;
 
-	uintptr_t localPlayer = mem.read<uintptr_t>(moduleBase + dwLocalPlayer);
+	uintptr_t localPlayer = mem.read<uintptr_t>(clientBase + dwLocalPlayer);
 	int crosshairPlayer = mem.read<int>(localPlayer + m_iCrosshairId);
 
+	if (!localPlayer)
+		return;
+
 	if (crosshairPlayer != 0 && crosshairPlayer < 64) {
-		uintptr_t player = mem.read<uintptr_t>(moduleBase + dwEntityList + (crosshairPlayer - 1) * 0x10);
+		uintptr_t player = mem.read<uintptr_t>(clientBase + dwEntityList + (crosshairPlayer - 1) * 0x10);
 		int playerTeam = mem.read<int>(player + m_iTeamNum);
 		int localPlayerTeam = mem.read<int>(localPlayer + m_iTeamNum);
 
@@ -27,6 +30,6 @@ void Triggerbot::run() {
 		if (mem.read<bool>(player + m_bGunGameImmunity))
 			return;
 
-		mem.write<DWORD>(moduleBase + dwForceAttack, 6);
+		mem.write<DWORD>(clientBase + dwForceAttack, 6);
 	}
 }
